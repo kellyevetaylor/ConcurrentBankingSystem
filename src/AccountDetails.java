152560c40b8701da8fecc1d138e04415ea1156f8
Andrew Mcinnes Marshall CS2015 <andrew.marshall.2015@uni.strath.ac.uk>
2017-10-06 12:41:17 +0100

    Creating joinaccount interfaces

--- src/AccountDetails.java
+++ src/AccountDetails.java
@@ -1,4 +1,26 @@
 
 public class AccountDetails {
 
+	String holderName;
+	String dateOfBirth;
+	String Address;
+	int AccountNo;
+	int sortCode;
+	
+	
+	
+	public void create(){
+		//create account for a user
+		
+	}
+	
+	public void delete(){
+		
+		
+	}
+	
+	public void edit(){
+		//edit details of accounts
+		
+	}
 }
---
+++ src/JointAccount.java
@@ -0,0 +1,11 @@
+
+public interface JointAccount {
+
+	void deposit(int amount);		// deposit amount given into account given
+	
+	int checkBalance();			// of account given
+		
+	String checkDetails();			//of account given
+	
+	void transfer();		//from accoutna to account b 
+}
