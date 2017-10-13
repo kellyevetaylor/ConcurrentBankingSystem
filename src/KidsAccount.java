9b117969820f646c352685831497d755cf5e8cca
Kelly Taylor CS2015 <kelly.taylor.2015@uni.strath.ac.uk>
2017-10-13 11:05:24 +0100

    Started working on kids and savings accounts
    Signed-off-by: Kelly Taylor CS2015 <kelly.taylor.2015@uni.strath.ac.uk>

---
+++ src/KidsAccount.java
@@ -0,0 +1,55 @@
+/**
+ * Account type: Kids.
+ * This is a savings account for kids but will be joint with a parent.
+ * May have some interest and a maximum withdraw.
+ */
+
+public class KidsAccount implements Account {
+
+    private double balance;
+    private String accountName;
+    private User accountHolder;
+    private boolean isJoint;
+    private double max;
+
+    KidsAccount(double balance, boolean isJoint, User accountHolder, String accountName, double max){
+        this.balance = balance;
+        this.accountName = accountName;
+        this.accountHolder = accountHolder;
+        this.max = max;
+    }
+
+    @Override
+    public void deposit(double amount) {
+        balance+= amount;
+    }
+
+    /**
+     * "Parent" enters maximum amount that's allowed to be withdrawn from
+     * kids account.    TODO anything over this should throw an error
+     * @return
+     */
+    public double maxWithdrawal(){
+        return max;
+    }
+
+    @Override
+    public void credit(double amount) {
+        balance-=amount;
+    }
+
+    @Override
+    public double getBalance() {
+        return balance;
+    }
+
+    @Override
+    public String getName() {
+        return accountName;
+    }
+
+    @Override
+    public void transfer() {
+
+    }
+}
---
+++ src/SavingsAccount.java
@@ -0,0 +1,45 @@
+/**
+ * Account type: Savings.
+ * This will allow the user to enter an amount and gain interest on it.
+ * Right now, it just mimics a current account.
+ */
+
+public class SavingsAccount implements Account {
+
+    private double balance;
+    private String accountName;
+    private User accountHolder;
+    private boolean isJoint;
+
+    SavingsAccount(double balance, boolean isJoint, User accountHolder, String accountName){
+        this.balance = balance;
+        this.isJoint = isJoint;
+        this.accountName = accountName;
+        this.accountHolder = accountHolder;
+    }
+
+    @Override
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+
+    @Override
+    public void credit(double amount) { balance -= amount;
+    }
+
+    @Override
+    public double getBalance() {
+        return balance;
+    }
+
+    @Override
+    public String getName() {
+        return accountName;
+    }
+
+    @Override
+    public void transfer() {
+
+    }
+}
